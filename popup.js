2c8212788cbbc84446a7aae4bb5568d2ee129396
bhowmikp <prantarbhowmik@yahoo.com>
2016-06-03 20:09:06 -0400

    Attempt communication with website

--- popup.js
+++ popup.js
@@ -1,3 +1,9 @@
 chrome.browserAction.setBadgeText({text: '1'});
 
 console.log('11');
+
+// communication with website
+document.getElementById("theButton").addEventListener("click",
+    function() {
+  window.postMessage({ type: "FROM_PAGE", text: "Hello from the webpage!" }, "*");
+}, false);
